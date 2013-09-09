243e515366ddaa78a8905498abe0978087a99466
Vincent Balat <vincent.balat@univ-paris-diderot.fr>
2013-09-09 12:49:33 +0200

    Removing functions from unix.js that are now in js_of_ocaml

--- js/unix.js
+++ js/unix.js
@@ -1,30 +1,3 @@
 function unix_inet_addr_of_string () {return 0;}
-function unix_time () {return 0;}
-function unix_gmtime () {return 0;}
 function caml_ml_output_char () {return 0;}
 function caml_get_exception_backtrace () {return 0;}
-
-
-function unix_gettimeofday () {
-  return (new Date()).getTime() / 1000;
-}
-
-function unix_gmtime (t) {
-  var d = new Date (t * 1000);
-  var januaryfirst = new Date(d.getUTCFullYear(), 0, 1);
-  var doy = Math.floor((d - januaryfirst) / 86400000);
-  return [0, d.getUTCSeconds(), d.getUTCMinutes(), d.getUTCHours(),
-          d.getUTCDate(), d.getUTCMonth(), d.getUTCFullYear() - 1900,
-          d.getUTCDay(), doy,
-          false /* for UTC daylight savings time is false */]
-}
-
-function unix_localtime (t) {
-var d = new Date (t * 1000);
-var januaryfirst = new Date(d.getFullYear(), 0, 1);
-var doy = Math.floor((d - januaryfirst) / 86400000);
-return [0, d.getSeconds(), d.getMinutes(), d.getHours(),
-d.getDate(), d.getMonth(), d.getFullYear() - 1900,
-d.getDay(), doy,
-false /* daylight savings time  field. Not implemented: not used. */]
-}
