768152bb340ed742ff7fe9e37b426f09285be2fc
Vincent Balat <vincent.balat@univ-paris-diderot.fr>
2014-09-16 19:41:08 +0200

    Remove dependency to eliom-widgets

--- .jenkins.sh
+++ .jenkins.sh
@@ -2,9 +2,7 @@ opam pin add --no-action eliom-base-app .
 opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
 opam pin add --no-action js_of_ocaml 'https://github.com/ocsigen/js_of_ocaml.git#master'
 opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git#master'
-opam pin add --no-action ojwidgets 'https://github.com/ocsigen/ojwidgets.git#master'
 opam pin add --no-action ojquery 'https://github.com/ocsigen/ojquery.git#master'
-opam pin add --no-action eliom-widgets 'https://github.com/ocsigen/eliom-widgets.git#master'
 opam pin add --no-action ocsigen-widgets 'https://github.com/ocsigen/ocsigen-widgets.git#master'
 opam install --deps-only eliom-base-app
 opam install ocsigen-widgets
