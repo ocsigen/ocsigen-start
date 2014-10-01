fa15c77872e1785abcb1c27a26d9bf3f4c9b13f1
hhugo <hugo.heuzard@gmail.com>
2014-10-01 10:12:19 +0200

    Update .jenkins.sh

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,8 +1,8 @@
 opam pin add --no-action eliom-base-app .
-opam pin add --no-action deriving 'https://github.com/ocsigen/deriving.git#master'
-opam pin add --no-action tyxml 'https://github.com/ocsigen/tyxml.git#master'
+#opam pin add --no-action deriving 'https://github.com/ocsigen/deriving.git#master'
+#opam pin add --no-action tyxml 'https://github.com/ocsigen/tyxml.git#master'
 opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
-opam pin add --no-action js_of_ocaml 'https://github.com/ocsigen/js_of_ocaml.git#master'
+#opam pin add --no-action js_of_ocaml 'https://github.com/ocsigen/js_of_ocaml.git#master'
 opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git#master'
 opam pin add --no-action ojquery 'https://github.com/ocsigen/ojquery.git#master'
 opam pin add --no-action ocsigen-widgets 'https://github.com/ocsigen/ocsigen-widgets.git#master'
