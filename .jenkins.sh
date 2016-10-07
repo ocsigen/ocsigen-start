a6811a78f02a980dd128301e87120628654b85c8
Danny Willems <contact@danny-willems.be>
2016-10-07 17:03:07 +0200

    .jenkins.sh: reorganize OPAM repository as in the README and add wikidoc as dependency

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,13 +1,13 @@
-opam pin add --no-action ocsigen-start .
-#opam pin add --no-action deriving 'https://github.com/ocsigen/deriving.git#master'
-opam pin add --no-action text 'https://github.com/vbmithr/ocaml-text.git#master'
 opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
 opam pin add --no-action js_of_ocaml 'https://github.com/ocsigen/js_of_ocaml.git#master'
-opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git#master'
+opam pin add --no-action deriving 'https://github.com/ocsigen/deriving.git'
+opam pin add --no-action reactiveData 'https://github.com/ocsigen/reactiveData.git#master'
+opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git'
+opam pin add --no-action ocsigen-toolkit 'https://github.com/ocsigen/ocsigen-toolkit.git#master'
 opam pin add --no-action ojquery 'https://github.com/ocsigen/ojquery.git#master'
 opam pin add --no-action ocsigen-widgets 'https://github.com/ocsigen/ocsigen-widgets.git#master'
-opam pin add --no-action ocsigen-toolkit 'https://github.com/ocsigen/ocsigen-toolkit.git#master'
-opam pin add --no-action reactiveData 'https://github.com/ocsigen/reactiveData.git#master'
+opam pin add --no-action wikidoc 'https://github.com/ocsigen/wikidoc.git#master'
+opam pin add --no-action ocsigen-start .
 
 opam install --deps-only ocsigen-start
 opam install --verbose ocsigen-start
