da23a49a145410e9a3a9ff584c7ed9e27da43891
Vasilis Papavasileiou <git@vasilis.airpost.net>
2016-11-16 16:17:35 +0100

    Use PPX-enabled eliompp for documentation (TEST)

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,6 +1,6 @@
 opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
 opam pin add --no-action reactiveData 'https://github.com/ocsigen/reactiveData.git#master'
-opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git'
+opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git#eliompp'
 opam pin add --no-action ocsigen-toolkit 'https://github.com/ocsigen/ocsigen-toolkit.git#master'
 opam pin add --no-action wikidoc 'https://github.com/ocsigen/wikidoc.git#master'
 opam pin add --no-action ocsigen-start .
